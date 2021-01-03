1d8f1191cd2490eaa1c74d968ac0606e71f038e5
stevenhughes08 <stevenhughes08@gmail.com>
2021-01-03 13:12:12 -0500

    update router/index.js to use Vue3 syntax

--- blog/src/router/index.js
+++ blog/src/router/index.js
@@ -1,11 +1,9 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+// import Vue from 'vue';
+import { createWebHistory, createRouter } from 'vue-router';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
 
-Vue.use(VueRouter);
-
-const routes = [
-    {
+const routes = [{
         path: '/',
         name: 'Home',
         component: Home
@@ -13,18 +11,44 @@ const routes = [
     {
         path: '/about',
         name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+        component: About
+    },
 ];
 
-const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
+
+
+
+// import VueRouter from 'vue-router';
+// import Home from '../views/Home.vue';
+
+// Vue.use(VueRouter);
+
+// const routes = [{
+//         path: '/',
+//         name: 'Home',
+//         component: Home
+//     },
+//     {
+//         path: '/about',
+//         name: 'About',
+//         // route level code-splitting
+//         // this generates a separate chunk (about.[hash].js) for this route
+//         // which is lazy-loaded when the route is visited.
+//         component: () =>
+//             import ( /* webpackChunkName: "about" */ '../views/About.vue')
+//     }
+// ];
+
+// const router = new VueRouter({
+//     mode: 'history',
+//     base: process.env.BASE_URL,
+//     routes
+// });
+
+// export default router;
